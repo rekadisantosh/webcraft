ccfa458df79ddea6b9d257c10386efd78dbfa678
harshasiva0 <harshasiva0@gmail.com>
2018-02-15 23:47:28 -0500

    modified_home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -39,10 +39,6 @@
   <h1 class="display-4">WELCOME</h1>
   <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
   <hr class="my-4">
-  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-  <p class="lead">
-    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-  </p>
 </div>
     <marquee behaviour="alternate" scrolldelay="10" direction="right"><h1 align="center">WELCOME TO OUR WEBSITE</h1></marquee>
     
